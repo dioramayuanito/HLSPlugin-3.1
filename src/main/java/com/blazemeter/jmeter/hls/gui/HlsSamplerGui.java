6d6574132702c3a8ae461d6ef681c10813643652
Dio <diorama@detik.com>
2024-05-11 09:58:52 +0700

    coba kirim lagi

--- src/main/java/com/blazemeter/jmeter/hls/gui/HlsSamplerGui.java
+++ src/main/java/com/blazemeter/jmeter/hls/gui/HlsSamplerGui.java
@@ -11,88 +11,88 @@ import org.apache.jmeter.testelement.TestElement;
 
 public class HlsSamplerGui extends AbstractSamplerGui {
 
-  private HlsSamplerPanel hlsSamplerPanel;
+    private HlsSamplerPanel hlsSamplerPanel;
 
-  public HlsSamplerGui() {
-    hlsSamplerPanel = new HlsSamplerPanel();
-    setLayout(new BorderLayout(0, 5));
-    setBorder(makeBorder());
-    add(makeTitlePanel(), BorderLayout.NORTH);
-    add(hlsSamplerPanel, BorderLayout.CENTER);
-  }
-
-  @VisibleForTesting
-  public HlsSamplerGui(HlsSamplerPanel panel) {
-    hlsSamplerPanel = panel;
-  }
-
-  @Override
-  public String getStaticLabel() {
-    return "bzm - Streaming Sampler";
-  }
-
-  @Override
-  public String getLabelResource() {
-    throw new IllegalStateException("This shouldn't be called");
-  }
-
-  @Override
-  public TestElement createTestElement() {
-    HlsSampler sampler = new HlsSampler();
-    modifyTestElement(sampler);
-    return sampler;
-  }
-
-  @Override
-  public void configure(TestElement el) {
-    super.configure(el);
-    HlsSampler sampler = (HlsSampler) el;
-    hlsSamplerPanel.setMasterUrl(sampler.getMasterUrl());
-    hlsSamplerPanel.setEzdrmDashPostUrl(sampler.getEzdrmDashPostUrl());
-    hlsSamplerPanel.setEzdrmDashPostData(sampler.getEzdrmDashPostData());
-    hlsSamplerPanel.setPlayVideoDuration(sampler.isPlayVideoDuration());
-    hlsSamplerPanel.setPlaySeconds(sampler.getPlaySeconds());
-    hlsSamplerPanel.setAudioLanguage(sampler.getAudioLanguage());
-    hlsSamplerPanel.setSubtitleLanguage(sampler.getSubtitleLanguage());
-    hlsSamplerPanel.setBandwidthSelector(sampler.getBandwidthSelector());
-    hlsSamplerPanel.setResolutionSelector(sampler.getResolutionSelector());
-    hlsSamplerPanel.setResumeStatus(sampler.getResumeVideoStatus());
-    hlsSamplerPanel.setProtocolSelector(sampler.getProtocolSelector());
-  }
-
-  @Override
-  public void modifyTestElement(TestElement s) {
-    this.configureTestElement(s);
-    if (s instanceof HlsSampler) {
-      HlsSampler sampler = (HlsSampler) s;
-      sampler.setMasterUrl(hlsSamplerPanel.getMasterUrl());
-      sampler.setEzdrmDashPostUrl(sampler.getEzdrmDashPostUrl());
-      sampler.setEzdrmDashPostData(sampler.getEzdrmDashPostData());
-      sampler.setPlayVideoDuration(hlsSamplerPanel.isPlayVideoDuration());
-      sampler.setPlaySeconds(hlsSamplerPanel.getPlaySeconds());
-      sampler.setAudioLanguage(hlsSamplerPanel.getAudioLanguage());
-      sampler.setSubtitleLanguage(hlsSamplerPanel.getSubtitleLanguage());
-      sampler.setBandwidthSelector(hlsSamplerPanel.getBandwidthSelector());
-      sampler.setResolutionSelector(hlsSamplerPanel.getResolutionSelector());
-      sampler.setResumeVideoStatus(hlsSamplerPanel.getResumeVideoStatus());
-      sampler.setProtocolSelector(hlsSamplerPanel.getProtocolSelector());
+    public HlsSamplerGui() {
+        hlsSamplerPanel = new HlsSamplerPanel();
+        setLayout(new BorderLayout(0, 5));
+        setBorder(makeBorder());
+        add(makeTitlePanel(), BorderLayout.NORTH);
+        add(hlsSamplerPanel, BorderLayout.CENTER);
     }
-  }
 
-  @Override
-  public void clearGui() {
-    super.clearGui();
-    hlsSamplerPanel.setMasterUrl("");
-    hlsSamplerPanel.setEzdrmDashPostUrl("");
-    hlsSamplerPanel.setEzdrmDashPostData("");
-    hlsSamplerPanel.setPlayVideoDuration(false);
-    hlsSamplerPanel.setPlaySeconds("");
-    hlsSamplerPanel.setAudioLanguage("");
-    hlsSamplerPanel.setSubtitleLanguage("");
-    hlsSamplerPanel.setBandwidthSelector(BandwidthSelector.MIN);
-    hlsSamplerPanel.setResolutionSelector(ResolutionSelector.MIN);
-    hlsSamplerPanel.setResumeStatus(false);
-    hlsSamplerPanel.setProtocolSelector(Protocol.AUTOMATIC);
-  }
+    @VisibleForTesting
+    public HlsSamplerGui(HlsSamplerPanel panel) {
+        hlsSamplerPanel = panel;
+    }
+
+    @Override
+    public String getStaticLabel() {
+        return "bzm - Streaming Sampler";
+    }
+
+    @Override
+    public String getLabelResource() {
+        throw new IllegalStateException("This shouldn't be called");
+    }
+
+    @Override
+    public TestElement createTestElement() {
+        HlsSampler sampler = new HlsSampler();
+        modifyTestElement(sampler);
+        return sampler;
+    }
+
+    @Override
+    public void configure(TestElement el) {
+        super.configure(el);
+        HlsSampler sampler = (HlsSampler) el;
+        hlsSamplerPanel.setMasterUrl(sampler.getMasterUrl());
+        hlsSamplerPanel.setEzdrmDashPostUrl(sampler.getEzdrmDashPostUrl());
+        hlsSamplerPanel.setEzdrmDashPostData(sampler.getEzdrmDashPostData());
+        hlsSamplerPanel.setPlayVideoDuration(sampler.isPlayVideoDuration());
+        hlsSamplerPanel.setPlaySeconds(sampler.getPlaySeconds());
+        hlsSamplerPanel.setAudioLanguage(sampler.getAudioLanguage());
+        hlsSamplerPanel.setSubtitleLanguage(sampler.getSubtitleLanguage());
+        hlsSamplerPanel.setBandwidthSelector(sampler.getBandwidthSelector());
+        hlsSamplerPanel.setResolutionSelector(sampler.getResolutionSelector());
+        hlsSamplerPanel.setResumeStatus(sampler.getResumeVideoStatus());
+        hlsSamplerPanel.setProtocolSelector(sampler.getProtocolSelector());
+    }
+
+    @Override
+    public void modifyTestElement(TestElement s) {
+        this.configureTestElement(s);
+        if (s instanceof HlsSampler) {
+            HlsSampler sampler = (HlsSampler) s;
+            sampler.setMasterUrl(hlsSamplerPanel.getMasterUrl());
+            sampler.setEzdrmDashPostUrl(hlsSamplerPanel.getEzdrmDashPostUrl());
+            sampler.setEzdrmDashPostData(hlsSamplerPanel.getEzdrmDashPostData());
+            sampler.setPlayVideoDuration(hlsSamplerPanel.isPlayVideoDuration());
+            sampler.setPlaySeconds(hlsSamplerPanel.getPlaySeconds());
+            sampler.setAudioLanguage(hlsSamplerPanel.getAudioLanguage());
+            sampler.setSubtitleLanguage(hlsSamplerPanel.getSubtitleLanguage());
+            sampler.setBandwidthSelector(hlsSamplerPanel.getBandwidthSelector());
+            sampler.setResolutionSelector(hlsSamplerPanel.getResolutionSelector());
+            sampler.setResumeVideoStatus(hlsSamplerPanel.getResumeVideoStatus());
+            sampler.setProtocolSelector(hlsSamplerPanel.getProtocolSelector());
+        }
+    }
+
+    @Override
+    public void clearGui() {
+        super.clearGui();
+        hlsSamplerPanel.setMasterUrl("");
+        hlsSamplerPanel.setEzdrmDashPostUrl("");
+        hlsSamplerPanel.setEzdrmDashPostData("");
+        hlsSamplerPanel.setPlayVideoDuration(false);
+        hlsSamplerPanel.setPlaySeconds("");
+        hlsSamplerPanel.setAudioLanguage("");
+        hlsSamplerPanel.setSubtitleLanguage("");
+        hlsSamplerPanel.setBandwidthSelector(BandwidthSelector.MIN);
+        hlsSamplerPanel.setResolutionSelector(ResolutionSelector.MIN);
+        hlsSamplerPanel.setResumeStatus(false);
+        hlsSamplerPanel.setProtocolSelector(Protocol.AUTOMATIC);
+    }
 
 }
